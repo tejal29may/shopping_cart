80b17a83d9076beb44ff67b1a51b177ee2ff5109
tejal29may <85352094+tejal29may@users.noreply.github.com>
2024-01-15 21:28:10 +0530

    shopping cart

---
+++ product.js
@@ -0,0 +1,9 @@
+// this js file is used to store the details about all the products
+const productList = [
+    {id: 1, name: "Promise Land", price:1300},
+    {id: 2, name: "The Illusion", price:450},
+    {id: 3, name: "Atomic Habits", price:300},
+    // {id: 4, name: "Product-4", price:400},
+    // {id: 5, name: "Product-5", price:500},
+    // {id: 6, name: "Product-6", price:600}
+];
